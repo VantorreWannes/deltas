6cd9922a9f1d76d0e6dd5f59b25ae41b05d11495
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-08-20 18:58:38 +0200

    Add instruction template files
    Create a folder/file structure to house all the needed code to implement anything to do with delta instructions

---
+++ src/delta_instructions/mod.rs
@@ -0,0 +1,5 @@
+pub mod traits;
+pub mod remove_instruction;
+pub mod instruction_enum;
+pub mod copy_instruction;
+pub mod add_instruction;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub mod delta_instructions;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
