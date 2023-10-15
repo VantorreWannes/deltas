d12b68f49322b9dbe6bf20e1685ed22c47b88629
JokNavi <JokNavi1@gmail.com>
2023-10-15 19:56:46 +0200

    Add is_full function to Instruction

--- src/instructions.rs
+++ src/instructions.rs
@@ -6,15 +6,18 @@ pub enum Instruction {
 }
 
 impl Instruction {
-
     pub fn len(&self) -> u8 {
         match self {
             Instruction::Remove { length } => *length,
-            Instruction::Add { content } | Instruction::Copy { content }  => content.len() as u8,
+            Instruction::Add { content } | Instruction::Copy { content } => content.len() as u8,
         }
     }
 
     pub fn is_empty(&self) -> bool {
         self.len() == 0
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.len() == u8::MAX
     }
 }
