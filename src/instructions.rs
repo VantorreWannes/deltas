f415f5ac34355f5c018bd94d349e412f36311e63
JokNavi <JokNavi1@gmail.com>
2023-10-15 19:45:25 +0200

    Add Instruction enum

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,4 +1,9 @@
 
-pub enum Instructions {
-    
+#[derive(Debug, Clone, PartialEq)]
+pub enum Instruction {
+    Remove { length: u8 },
+    Add { content: Vec<u8> },
+    Copy { content: Vec<u8> },
 }
+
+
