0e1aafbcb2cb8b0045e0f10f972a357711c597cf
JokNavi <JokNavi1@gmail.com>
2023-10-16 15:06:28 +0200

    Add byte_len() and to_bytes() fn to Patch

--- src/patch.rs
+++ src/patch.rs
@@ -1,14 +1,30 @@
 use crate::instructions::Instruction;
 
-
 #[derive(Debug, Clone, PartialEq)]
 pub struct Patch {
     content: Vec<Instruction>,
 }
 
 impl Patch {
-
     pub fn new(source: &[u8], target: &[u8]) -> Self {
         todo!();
+    }
+
+    fn byte_len(&self) -> usize {
+        self.content
+            .iter()
+            .map(|instruction| match instruction {
+                Instruction::Remove { length } => 2,
+                Instruction::Add { content } | Instruction::Copy { content } => content.len() + 2,
+            })
+            .sum()
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes: Vec<u8> = Vec::with_capacity(self.byte_len());
+        for instruction in self.content.iter() {
+            bytes.append(&mut instruction.into());
+        }
+        bytes
     }
 }
