9e8ce80142174f2aa55b938c05b15d70d145e03e
JokNavi <JokNavi1@gmail.com>
2023-10-17 19:42:17 +0200

    Add InstructionItem and instructions::Result<T> type alias

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,3 +1,5 @@
+use self::error::InstructionError;
+
 pub mod instruction;
 pub mod error;
 pub mod traits;
@@ -5,7 +7,11 @@ mod add_instruction;
 mod copy_instruction;
 mod remove_instruction;
 
+type InstructionItem = u8;
 type InstructionLength = u8;
+
+type Result<T> = std::result::Result<T, InstructionError>;
+
 const MAX_INSTRUCTION_LENGTH: u8 = InstructionLength::MAX;
 const MIN_INSTRUCTION_LENGTH: u8 = InstructionLength::MIN;
 
