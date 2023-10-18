d99a75b5378b1f8a5f936ddad32b391bab9f17b7
JokNavi <JokNavi1@gmail.com>
2023-10-18 19:33:26 +0200

    Impl InstructionContent for DeltaInstruction

--- src/instructions/delta_instruction.rs
+++ src/instructions/delta_instruction.rs
@@ -1,4 +1,7 @@
-use super::{remove_instruction::RemoveInstruction, add_instruction::AddInstruction, copy_instruction::CopyInstruction, InstructionInfo};
+use super::{
+    add_instruction::AddInstruction, copy_instruction::CopyInstruction,
+    remove_instruction::RemoveInstruction, InstructionContent, InstructionInfo, Result,
+};
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum DeltaInstruction {
@@ -37,6 +40,16 @@ impl InstructionInfo for DeltaInstruction {
             DeltaInstruction::Remove(instruction) => instruction.non_default_item_count(),
             DeltaInstruction::Add(instruction) => instruction.non_default_item_count(),
             DeltaInstruction::Copy(instruction) => instruction.non_default_item_count(),
+        }
+    }
+}
+
+impl InstructionContent for DeltaInstruction {
+    fn push(&mut self, content: super::InstructionItem) -> Result<()> {
+        match self {
+            DeltaInstruction::Remove(instruction) => instruction.push(content),
+            DeltaInstruction::Add(instruction) => instruction.push(content),
+            DeltaInstruction::Copy(instruction) => instruction.push(content),
         }
     }
 }
