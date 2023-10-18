e88e7c6c36fc33f2203a0b7cf16b5058e38097fa
JokNavi Mora <JokNavi1@gmail.com>
2023-10-18 09:57:20 +0200

    Add new InstructionError varriants to cover the error cases of the InstructionBytes trait

--- src/instructions/error.rs
+++ src/instructions/error.rs
@@ -2,17 +2,35 @@ use std::error::Error;
 
 use crate::instructions::InstructionLength;
 
+use super::{ADD_INSTRUCTION_SIGN, COPY_INSTRUCTION_SIGN, REMOVE_INSTRUCTION_SIGN};
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum InstructionError {
     ContentOverflow,
+    MissignSign,
+    InvalidSign,
+    MissingLength,
+    MissingContent,
 }
 
 impl std::fmt::Display for InstructionError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             InstructionError::ContentOverflow => {
-                write!(f, "Cannot exceed {} amount of bytes in an instruction", InstructionLength::MAX)
+                write!(
+                    f,
+                    "Cannot exceed {} amount of bytes in an instruction",
+                    InstructionLength::MAX
+                )
             }
+            InstructionError::MissignSign => write!(f, "No instruction sign found"),
+            InstructionError::InvalidSign => write!(
+                f,
+                "Instruction sign didn't match: {}, {} or {}",
+                REMOVE_INSTRUCTION_SIGN, ADD_INSTRUCTION_SIGN, COPY_INSTRUCTION_SIGN
+            ),
+            InstructionError::MissingLength => write!(f, "No length value found"),
+            InstructionError::MissingContent => write!(f, "Not enough bytes found to match the given length"),
         }
     }
 }
