e98a6ae52c1facff9c64fd1bee93400115386eff
JokNavi Mora <JokNavi1@gmail.com>
2023-10-18 12:59:32 +0200

    Impl InstructionBytes trait for CopyInstruction

--- src/instructions/copy_instruction.rs
+++ src/instructions/copy_instruction.rs
@@ -1,4 +1,6 @@
-use super::{InstructionItem, InstructionLength, traits::{InstructionInfo, InstructionContent}, error::InstructionError, Result};
+use std::{iter::Peekable, slice::Iter};
+
+use super::{InstructionItem, InstructionLength, traits::{InstructionInfo, InstructionContent, InstructionBytes}, error::InstructionError, Result, COPY_INSTRUCTION_SIGN};
 
 
 #[derive(Debug, PartialEq, Clone, Default)]
@@ -42,4 +44,65 @@ impl InstructionContent for CopyInstruction {
     }
 }
 
+impl InstructionBytes for CopyInstruction {
+    fn byte_sign() -> u8 {
+        COPY_INSTRUCTION_SIGN
+    }
+
+    fn byte_length(&self) -> usize {
+        usize::try_from(self.len()).unwrap() + std::mem::size_of::<InstructionLength>() + 1
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes: Vec<u8> = Vec::with_capacity(self.byte_length());
+            bytes.push(CopyInstruction::byte_sign());
+            bytes.extend(self.len().to_be_bytes());
+            bytes.extend(self.content.iter());
+            bytes
+    }
+
+    fn try_from_bytes(bytes: &mut Peekable<Iter<'_, u8>>) -> Result<Self> {
+        match bytes.next() {
+            Some(&COPY_INSTRUCTION_SIGN) => (),
+            Some(_) => return Err(InstructionError::InvalidSign),
+            None => return Err(InstructionError::MissignSign),
+        };
+
+        if bytes.peek().is_none() {
+            return Err(InstructionError::MissingLength);
+        }
+
+        let length_bytes: Vec<u8> = bytes
+            .take(std::mem::size_of::<InstructionLength>())
+            .copied()
+            .collect();
+        let length = InstructionLength::from_be_bytes(length_bytes.as_slice().try_into().map_err(|_| InstructionError::InvalidLength)?);
+
+        let content_bytes: Vec<u8> = bytes
+            .take(
+                length.try_into().unwrap()
+            )
+            .copied()
+            .collect();
+
+        let content: Result<Vec<InstructionItem>> = content_bytes
+            .chunks(std::mem::size_of::<InstructionItem>())
+            .map(|chunk: &[u8]| -> Result<InstructionItem> {
+                Ok(InstructionItem::from_be_bytes(
+                    chunk
+                        .try_into()
+                        .map_err(|_| InstructionError::InvalidContent)?,
+                ))
+            })
+            .collect();
+
+        let content = content?;
+
+        if content.len() < length as usize {
+            return Err(InstructionError::MissingContent);
+        }
+
+        Ok(Self { content })
+    }
+}
 
